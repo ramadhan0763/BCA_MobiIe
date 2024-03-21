671af2ab9d849f574561485a34dd471f8bbef2ce
Lamz16 <andiku0755@gmail.com>
2024-03-21 13:34:56 +0700

    layoutWelcome Done

--- app/src/main/java/com/bank/bcamobiie/WelcomeActivity.kt
+++ app/src/main/java/com/bank/bcamobiie/WelcomeActivity.kt
@@ -2,10 +2,18 @@ package com.bank.bcamobiie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bank.bcamobiie.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private var _binding : ActivityWelcomeBinding? = null
+    private val binding : ActivityWelcomeBinding get() =  _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        _binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
     }
 }
